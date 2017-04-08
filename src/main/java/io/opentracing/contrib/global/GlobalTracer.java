214eb3801b307c28c20bcaeba5fb92e685ec3030
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-04-08 16:16:37 +0200

    Deprecate contrib GlobalTracer and forward to OpenTracing-java util.

--- src/main/java/io/opentracing/contrib/global/GlobalTracer.java
+++ src/main/java/io/opentracing/contrib/global/GlobalTracer.java
@@ -13,14 +13,14 @@
  */
 package io.opentracing.contrib.global;
 
+import io.opentracing.NoopTracer;
 import io.opentracing.NoopTracerFactory;
-import io.opentracing.SpanContext;
 import io.opentracing.Tracer;
-import io.opentracing.propagation.Format;
 
+import java.lang.reflect.Field;
 import java.util.Iterator;
 import java.util.ServiceLoader;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,43 +38,26 @@ import java.util.logging.Logger;
  * The {@linkplain GlobalTracer} will not attempt to choose between implementations:</li>
  * <li>If no single tracer service is found, the {@link io.opentracing.NoopTracer NoopTracer} will be used.</li>
  * </ol>
+ *
+ * @see io.opentracing.util.GlobalTracer
+ * @deprecated GlobalTracer is now part of OpenTracing-java
  */
-public final class GlobalTracer implements Tracer {
+public final class GlobalTracer {
     private static final Logger LOGGER = Logger.getLogger(GlobalTracer.class.getName());
 
-    /**
-     * Singleton instance.
-     * <p>
-     * Since we cannot prevent people using {@linkplain #get() GlobalTracer.get()} as a constant,
-     * this guarantees that references obtained before, during or after initialization
-     * all behave as if obtained <em>after</em> initialization once properly initialized.<br>
-     * As a minor additional benefit it makes it harder to circumvent the {@link Tracer} API.
-     */
-    private static final GlobalTracer INSTANCE = new GlobalTracer();
-
-    /**
-     * The resolved {@link Tracer} to delegate to.
-     * <p>
-     * This can be either an {@link #register(Tracer) explicitly registered} or
-     * the {@link #loadSingleSpiImplementation() automatically resolved} Tracer
-     * (or <code>null</code> during initialization).
-     */
-    private final AtomicReference<Tracer> globalTracer = new AtomicReference<Tracer>();
+    private static final AtomicBoolean SINGLE_INIT = new AtomicBoolean(false);
 
     private GlobalTracer() {
     }
 
-    private Tracer lazyTracer() {
-        Tracer tracer = globalTracer.get();
-        if (tracer == null) {
+    private static void lazyInit() {
+        if (SINGLE_INIT.compareAndSet(false, true) && tryReflectCurrent() instanceof NoopTracer) {
             final Tracer resolved = loadSingleSpiImplementation();
-            while (tracer == null && resolved != null) { // handle rare race condition
-                globalTracer.compareAndSet(null, resolved);
-                tracer = globalTracer.get();
+            if (!(resolved instanceof NoopTracer)) {
+                io.opentracing.util.GlobalTracer.register(resolved);
             }
-            LOGGER.log(Level.INFO, "Using GlobalTracer: {0}.", tracer);
+            LOGGER.log(Level.INFO, "Using GlobalTracer: {0}.", resolved);
         }
-        return tracer;
     }
 
     /**
@@ -88,10 +71,30 @@ public final class GlobalTracer implements Tracer {
      * For example, the tracer used to extract a span may be different than the one that injects it.
      *
      * @return The global tracer constant.
-     * @see #register(Tracer)
+     * @see io.opentracing.util.GlobalTracer#get()
+     * @deprecated Please use GlobalTracer from opentracing-util instead.
      */
     public static Tracer get() {
-        return INSTANCE;
+        lazyInit();
+        return io.opentracing.util.GlobalTracer.get();
+    }
+
+    private static Tracer tryReflectCurrent() {
+        Tracer current = null;
+        try {
+            Field tracerFld = io.opentracing.util.GlobalTracer.class.getDeclaredField("tracer");
+            synchronized (tracerFld) {
+                tracerFld.setAccessible(true);
+                try {
+                    current = (Tracer) tracerFld.get(null);
+                } finally {
+                    tracerFld.setAccessible(false);
+                }
+            }
+        } catch (Exception reflectionError) {
+
+        }
+        return current;
     }
 
     /**
@@ -103,28 +106,10 @@ public final class GlobalTracer implements Tracer {
      * @return The previous global tracer or <code>null</code> if there was none.
      */
     public static Tracer register(final Tracer tracer) {
-        if (tracer instanceof GlobalTracer) {
-            LOGGER.log(Level.FINE, "Attempted to register the GlobalTracer as delegate of itself.");
-            return INSTANCE.globalTracer.get(); // no-op, return 'previous' tracer.
-        }
-        Tracer previous = INSTANCE.globalTracer.getAndSet(tracer);
+        final Tracer previous = tryReflectCurrent();
+        io.opentracing.util.GlobalTracer.register(tracer);
         LOGGER.log(Level.INFO, "Registered GlobalTracer {0} (previously {1}).", new Object[]{tracer, previous});
         return previous;
-    }
-
-    @Override
-    public SpanBuilder buildSpan(String operationName) {
-        return lazyTracer().buildSpan(operationName);
-    }
-
-    @Override
-    public <C> void inject(SpanContext spanContext, Format<C> format, C carrier) {
-        lazyTracer().inject(spanContext, format, carrier);
-    }
-
-    @Override
-    public <C> SpanContext extract(Format<C> format, C carrier) {
-        return lazyTracer().extract(format, carrier);
     }
 
     /**
